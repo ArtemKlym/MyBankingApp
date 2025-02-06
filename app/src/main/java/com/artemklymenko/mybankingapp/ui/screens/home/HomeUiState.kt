dba53054b50a5445e87f2c147e1f628fb907064a
Artem <artemklymenko892@gmail.com>
2025-02-06 10:41:05 +0200

    Delete unnecessary field

--- app/src/main/java/com/artemklymenko/mybankingapp/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/com/artemklymenko/mybankingapp/ui/screens/home/HomeUiState.kt
@@ -1,12 +1,10 @@
 package com.artemklymenko.mybankingapp.ui.screens.home
 
 import com.artemklymenko.mybankingapp.ui.screens.home.data.Currency
-import com.artemklymenko.mybankingapp.ui.screens.home.data.ExchangeRate
 
 data class HomeUiState (
     val currenciesInDepartments: List<Currency> = emptyList(),
     val currenciesInCards: List<Currency> = emptyList(),
-    val exchangeRates: List<ExchangeRate> = emptyList(),
     val isLoading: Boolean = false,
     val error: String? = null
 )
